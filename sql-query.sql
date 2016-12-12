a7ccc2de4517e34ecca2831cf451b206ce7c03f3
Максим Попсуйко < >
2016-12-12 13:56:01 +0300

    edit reports and procedures

--- sql-query.sql
+++ sql-query.sql
@@ -2,7 +2,8 @@
 select car.id as car_id from car
 join car_equipment on car.id_equipment=car_equipment.id
 join sells on sells.id_car=car.id
-where car_equipment.name='Дорогая'
+where car_equipment.id=(select car_equipment.id
+from(select first 1 car_equipment.id, car_equipment.price from car_equipment order by price desc))
 and sells.sell_date between (dateadd(year, -5, current_timestamp)) and current_date;
 
 -- Запрос 2
@@ -23,6 +24,8 @@ select gear_box.id, gear_box.name, count(gear_box.id) as total_count from gear_b
 join car_modification on car_modification.gear_box=gear_box.id
 join car on car.id_modification=car_modification.id
 join car_equipment on car_equipment.id=car.id_equipment
-where car_equipment.name='Дешевая'
+where car_equipment.id=(select car_equipment.id
+from(select first 1 car_equipment.id, car_equipment.price from car_equipment order by price))
 group by gear_box.id,gear_box.name
 order by total_count desc;
+
